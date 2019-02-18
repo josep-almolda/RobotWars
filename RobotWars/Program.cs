3ee4c9d9a8cc7f6c8bf10cf3453e5d43da12aa12
josep <josep.almolda@gmail.com>
2019-02-18 16:30:05 +0000

    program cs

---
+++ RobotWars/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using RobotWars.Utils;
+
+namespace RobotWars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = string.Empty;
+            var currentLine = Console.ReadLine();
+            while (currentLine != "S")
+            {
+                input += $"{currentLine}\n";
+                currentLine = Console.ReadLine();
+            }
+            Console.WriteLine(input);
+            var robotWars = new RobotWars(new Parser());
+            var result = robotWars.Process(input);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }        
+    }
+}
