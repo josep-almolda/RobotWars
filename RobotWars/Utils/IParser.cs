798a7a6bb1d16beed3182f91515e436a9e89d1f3
josep <josep.almolda@gmail.com>
2019-02-18 16:31:04 +0000

    add utils and general class

---
+++ RobotWars/Utils/IParser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RobotWars.Commands;
+using RobotWars.Models;
+
+namespace RobotWars.Utils
+{
+    public interface IParser
+    {
+        Dimensions ParseDimensions(string input);
+        Position ParsePosition(string input);
+        List<ICommand> ParseCommamnds(string input);
+        string FormatPositions(List<Position> reportRobotsPositions);
+    }
+}
---
+++ RobotWars/Utils/Parser.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RobotWars.Commands;
+using RobotWars.Models;
+
+namespace RobotWars.Utils
+{
+    public class Parser : IParser
+    {
+        public Dimensions ParseDimensions(string input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Position ParsePosition(string input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ICommand> ParseCommamnds(string input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string FormatPositions(List<Position> reportRobotsPositions)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
