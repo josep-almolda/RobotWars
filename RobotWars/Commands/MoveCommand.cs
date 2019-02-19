257f8e9777d173a893b3d6320da4daab02f887a6
josep <josep.almolda@gmail.com>
2019-02-19 08:49:05 +0000

    implement move command

--- RobotWars/Commands/MoveCommand.cs
+++ RobotWars/Commands/MoveCommand.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using RobotWars.Models;
 
@@ -7,9 +8,45 @@ namespace RobotWars.Commands
 {
     public class MoveCommand : ICommand
     {
+        private bool IsOutOfArena(Position position, Dimensions dimensions)
+        {
+            return position.X > dimensions.X ||
+                   position.Y > dimensions.X ||
+                   position.X < 0 ||
+                   position.Y < 0;
+        }
+
+        private bool IsARobotClash(Position position, List<Robot> robots)
+        {
+            return robots.Any(r => r.Report().X == position.X && r.Report().Y == position.Y);
+        }
+
         public Position Execute(Position position, List<Robot> robots, Dimensions dimensions)
         {
-            throw new NotImplementedException();
+            var newPosition = new Position(position.X, position.Y, position.Heading);
+
+            switch (position.Heading)
+            {
+                case Heading.E:
+                    newPosition = new Position(position.X + 1, position.Y, position.Heading);
+                    break;
+                case Heading.S:
+                    newPosition = new Position(position.X, position.Y - 1, position.Heading);
+                    break;
+                case Heading.N:
+                    newPosition = new Position(position.X, position.Y + 1, position.Heading);
+                    break;
+                case Heading.W:
+                    newPosition = new Position(position.X - 1, position.Y, position.Heading);
+                    break;
+            }
+
+            if (IsOutOfArena(newPosition, dimensions) || IsARobotClash(newPosition, robots))
+            {
+                return position;
+            }
+
+            return newPosition;
         }
     }
 }
